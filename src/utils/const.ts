5475427edb6c81ccdc75e0adacb453526001bb87
Tony <56505654+purr-purr@users.noreply.github.com>
2024-06-30 23:09:29 +0100

    feature: upd info

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -13,7 +13,6 @@ export const COMPANY_SCHEDULE: string =
 	'Робочі дні: понелілок – п’ятниця з 9:00 до 18:00 год. ' +
 	'Bихідні: субота - неділя';
 export const COMPANY_ADDRESS: string =
-	'Україна, 03056, місто Київ, вулиця Польова (Солом\'янський Р-Н), будинок' +
-	' 24Д';
+	'Україна, 79024, Львівська область, місто Львів, вул. Промислова, будинок 60, літера «Б-3» кімната, №1';
 export const COMPANY_MAP_LINK: string =
-	'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d68398.20685293859!2d30.42730031963909!3d50.42501348422967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cdedf043b4b5%3A0x10d934d8d72983a2!2z0JHQpiBHcmFuZCBTdGVw!5e0!3m2!1sru!2sie!4v1707759646181!5m2!1sru!2sie';
+	'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1050.5635931120141!2d24.043778381149814!3d49.86327034746825!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473add0285541d65%3A0xd02190fd490a36aa!2z0LLRg9C70LjRhtGPINCf0YDQvtC80LjRgdC70L7QstCwLCA2MCwgMywg0JvRjNCy0ZbQsiwg0JvRjNCy0ZbQstGB0YzQutCwINC-0LHQu9Cw0YHRgtGMLCDQo9C60YDQsNGX0L3QsCwgNzkwMDA!5e0!3m2!1suk!2sie!4v1719785053619!5m2!1suk!2sie';
